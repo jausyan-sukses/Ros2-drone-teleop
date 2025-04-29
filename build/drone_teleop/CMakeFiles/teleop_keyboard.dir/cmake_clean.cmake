ec72cf6dd77f5c859e5f907029a99d847bbc51d8
jausyan-best <aptmeranti305@gmail.com>
2025-04-29 15:24:52 +0700

    Initial commit

---
+++ build/drone_teleop/CMakeFiles/teleop_keyboard.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/teleop_keyboard.dir/src/teleop_keyboard.cpp.o"
+  "teleop_keyboard"
+  "teleop_keyboard.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/teleop_keyboard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
